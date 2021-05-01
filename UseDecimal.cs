0add41031a379d8d58fdcedc721cb4b27b0f2531
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-05-01 22:24:26 +0300

    Lear how use type decimal

---
+++ UseDecimal.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price = 19.95m;
+            decimal discount = 0.15m;
+            decimal discountPrice = price - (price * discount);
+            Console.WriteLine("Price with discount: $" + discountPrice);
+        }
+    }
+}
