03f2e5a39a028211367ae563961e6564a3af22c0
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-05-02 19:59:33 +0300

    Learn ternar operator

---
+++ BasicOfProgramming/Operators/NoZeroDiv.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class NoZeroDiv
+    {
+        static void Main()
+        {
+            int result;
+            for (int i = -5; i < 6; i++)
+            {
+                result = i != 0 ? 100 / i : 0;
+                if (i != 0)
+                    Console.WriteLine("100 / " + i + " = " + result);
+            }
+        }
+    }
+}
