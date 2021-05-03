c2e0c0ab60eba165ac4d4dbdec5c770de7c89675
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-05-03 12:04:45 +0300

    Learn loop for

---
+++ BasicOfProgramming/ControlOperators/DecrFor.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class DecrFor
+    {
+        static void Main()
+        {
+            for (int x = 100; x > -100; x -= 5)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
