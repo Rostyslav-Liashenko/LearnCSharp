0d08bc589dade8c550e3a339dc6a06fd4c5bf282
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-05-03 12:50:39 +0300

    Learn operatorcontinue

---
+++ BasicOfProgramming/ControlOperators/ContDemo.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class ContDemo
+    {
+        static void Main()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                if ((i % 2) != 0)
+                    continue;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
