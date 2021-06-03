b95c947142f3b2ae089f35ea9de2c0409ee119a8
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-06-03 17:57:47 +0300

    Learn how get access to private members class with help properties

--- OOP/Inheritance/SimpleInheritance/Shape/TwoDShape.cs
+++ OOP/Inheritance/SimpleInheritance/Shape/TwoDShape.cs
@@ -4,8 +4,32 @@ namespace ConsoleApp6.Shape
 {
     public class TwoDShape
     {
-        public double Width;
-        public double Height;
+        private double priWidth;
+        private double priHeight;
+
+        public double Width
+        {
+            get
+            {
+                return priWidth;
+            }
+            set
+            {
+                priWidth = value < 0 ? -value : value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return priHeight;
+            }
+            set
+            {
+                priHeight = value < 0 ? -value : value;
+            }
+        }
         
         public void ShowDim()
         {
