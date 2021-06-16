98425f297c1f3a5a2338ee5b68c1e9899604ff6c
Rostyslav Liashenko <kivi27072002@gmail.com>
2021-06-16 17:59:50 +0300

    Oops! I forget class for this program

---
+++ OOP/Exception/SimpleException/NullReferenceException/X.cs
@@ -0,0 +1,17 @@
+namespace LearnException
+{
+    public class X
+    {
+        private int x;
+
+        public X(int a)
+        {
+            x = a;
+        }
+
+        public int Add(X o)
+        {
+            return x + o.x;
+        }
+    }
+}
