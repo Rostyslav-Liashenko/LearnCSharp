464709d420ef3643eb23f1c66e6e8a5018968c60
Kivi27 <kivi27072002@gmail.com>
2021-06-24 02:57:14 +0300

    Begin learn Input and Output

---
+++ BasicOfProgramming/InpAndOut/ReadString.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace LearnInpAndOut
+{
+    class ReadString
+    {
+        static void Main()
+        {
+            Console.WriteLine("Input the string of symbols:");
+            string str = Console.ReadLine();
+            Console.WriteLine("You written: " + str);
+        }
+    }
+}
