d3f63a09c7a840ee45125d2e94a6b800b0b12668
Kivi27 <kivi27072002@gmail.com>
2021-06-28 19:37:06 +0300

    Learn delegate

---
+++ OOP/Delegate/DelegateTest.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace LearnDelegate
+{
+    class DelegateTest
+    {
+        delegate string StrMod(string str);
+
+        static string ReplaceSpaces(string s)
+        {
+            Console.WriteLine("Replace space");
+            return s.Replace(' ','-');
+        }
+
+        static string RemoveSpaces(string s)
+        {
+            string temp = "";
+            int i;
+            Console.WriteLine("Delete spaces");
+            for (i = 0; i < s.Length; i++)
+                if (s[i] != ' ')
+                    temp += s[i];
+            return temp;
+        }
+
+        static string Reverse(string s)
+        {
+            string temp = "";
+            int i, j;
+            Console.WriteLine("Reverse string");
+            for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+                temp += s[i];
+            return temp;
+        }
+        
+        static void Main()
+        {
+            StrMod strOp = new StrMod(ReplaceSpaces);
+            string str;
+            str = strOp("It is simple test");
+            Console.WriteLine("Result: " + str);
+            Console.WriteLine();
+            strOp = new StrMod(RemoveSpaces);
+            str = strOp("It is simple test");
+            Console.WriteLine("Result: " + str);
+            Console.WriteLine();
+            strOp = new StrMod(Reverse);
+            str = strOp("It is simple test");
+            Console.WriteLine("Result: " + str);
+        }
+    }
+}
