e428340cbe4c006d6d73b8b456ee9ca549e2cf89
Kivi27 <kivi27072002@gmail.com>
2021-07-06 23:23:15 +0300

    Learn alias

---
+++ Other/NameSpace/LearnSecondFormUsing/NSDemo4.cs
@@ -0,0 +1,35 @@
+using System;
+using MyCounter = Counter.CountDown;
+
+
+namespace LearnNameSpace
+{
+    class NSDemo4
+    {
+        static void Main(string[] args)
+        {
+            MyCounter cd1 = new MyCounter(10);
+            int i;
+            do
+            {
+                i = cd1.Count();
+                Console.Write(i + " ");
+            } while (i > 0);
+            Console.WriteLine();
+            MyCounter cd2 = new MyCounter(20);
+            do
+            {
+                i = cd2.Count();
+                Console.Write(i + " ");
+            } while (i > 0);
+            Console.WriteLine();
+            cd2.Reset(4);
+            do
+            {
+                i = cd2.Count();
+                Console.Write(i + " ");
+            } while (i > 0);
+            Console.WriteLine();
+        }
+    }
+}
