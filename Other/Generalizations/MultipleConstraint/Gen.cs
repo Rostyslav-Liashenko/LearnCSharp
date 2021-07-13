b95b4d5e8c3ba519ba06b694e0b84b7537e14675
Kivi27 <kivi27072002@gmail.com>
2021-07-13 22:39:39 +0300

    Learn multiple constraint in generalizations

---
+++ Other/Generalizations/MultipleConstraint/Gen.cs
@@ -0,0 +1,15 @@
+namespace LearnGeneralization
+{
+    public class Gen<T, V> where T : class
+                            where V : struct
+    {
+        private T ob1;
+        private V ob2;
+
+        public Gen(T t, V v)
+        {
+            ob1 = t;
+            ob2 = v;
+        }
+    }
+}
---
+++ Other/Generalizations/MultipleConstraint/MultipleConstraintDemo.cs
@@ -0,0 +1,11 @@
+namespace LearnGeneralization
+{
+    class MultipleConstraintDemo
+    {
+        static void Main()
+        {
+            Gen<string, int> obj = new Gen<string, int>("test",11);
+            
+        }
+    }
+}
