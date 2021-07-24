07e3987b6a02f50321212dc59c39d1bebe46577f
Kivi27 <kivi27072002@gmail.com>
2021-07-24 22:06:55 +0300

    Learn delegate action, and how use with array

---
+++ Library.Net/DelegateAction/ActionDemo.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace LearnLibrary
+{
+    class ActionDemo
+    {
+        static void Show(MyClass o)
+        {
+            Console.Write(o.Num + " ");
+        }
+
+        static void Neg(MyClass o)
+        {
+            o.Num = -o.Num;
+        }
+
+        static void Main()
+        {
+            MyClass[] nums =
+            {
+                new MyClass(5),
+                new MyClass(2),
+                new MyClass(3),
+                new MyClass(4),
+                new MyClass(1)
+            };
+            Array.ForEach(nums,Show);
+            Console.WriteLine();
+            Array.ForEach(nums,Neg);
+            Console.Write("Array nums: ");
+            Array.ForEach(nums,Show);
+        }
+    }
+}
