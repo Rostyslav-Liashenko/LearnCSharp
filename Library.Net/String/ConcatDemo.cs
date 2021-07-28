4b71ee813418d295d53f7ced3ed8647e3e78a73f
Kivi27 <kivi27072002@gmail.com>
2021-07-28 21:47:57 +0300

    Learn method concat class String

---
+++ Library.Net/String/ConcatDemo.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace LearnLibrary
+{
+    class ConcatDemo
+    {
+        static void Main()
+        {
+            string result = String.Concat("It ", " is ", " method ", "class ", "String concat");
+            Console.WriteLine("Result: " +  result);
+        }
+    }
+}
