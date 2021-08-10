6a1a1d563c52e96d310137ea79ff537d99911f5e
Kivi27 <kivi27072002@gmail.com>
2021-08-10 20:52:44 +0300

    Learn how use method wait in task

---
+++ MultiThreading/TPL/Task/MethodWait/DemoWait.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearnTPL
+{
+    class DemoWait
+    {
+        static void MyTask()
+        {
+            Console.WriteLine("MyTask() #" + Task.CurrentId + " is start");
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In method MyTask() #" + Task.CurrentId + ", count = " + i);
+            }
+            Console.WriteLine("MyTask #" + Task.CurrentId + " is end");
+        }
+        
+        static void Main()
+        {
+            Task tsk = new Task(MyTask);
+            Task tsk2 = new Task(MyTask);
+            tsk.Start();
+            tsk2.Start();
+            Console.WriteLine("ID task #1 = " + tsk.Id);
+            Console.WriteLine("ID task #2 = " + tsk2.Id);
+            tsk.Wait();
+            tsk2.Wait();
+            Console.WriteLine("Main thread is finished");
+        }
+    }
+}
