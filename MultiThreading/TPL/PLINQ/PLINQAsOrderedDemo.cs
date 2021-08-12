f61ac7afe7aea89e3490f44b4d32fd316b827c5c
Kivi27 <kivi27072002@gmail.com>
2021-08-12 16:36:06 +0300

    Learn new method AsOrdered

---
+++ MultiThreading/TPL/PLINQ/PLINQAsOrderedDemo.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Linq;
+
+namespace LearnPLINQ
+{
+    class PLINQAsOrderedDemo
+    {
+        static void Main()
+        {
+            int[] data = new int[10000000];
+            for (int i = 0; i < data.Length; i++)
+                data[i] = i;
+            data[1000] = -1;
+            data[14000] = -2;
+            data[15000] = -3;
+            data[676000] = -4;
+            data[8024540] = -5;
+            data[9908000] = -6;
+            var negatives = from val in data.AsParallel().AsOrdered()
+                where val < 0
+                select val;
+            foreach (var v in negatives)
+                Console.Write(v + " ");
+            Console.WriteLine();
+        }
+    }
+}
