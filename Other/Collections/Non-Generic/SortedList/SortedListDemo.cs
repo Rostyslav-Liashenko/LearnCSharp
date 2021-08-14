4c4e727c600b931bd5806c3b1a3b25bffa3af153
Kivi27 <kivi27072002@gmail.com>
2021-08-14 16:12:12 +0300

    Learn some about sorted list

---
+++ Other/Collections/Non-Generic/SortedList/SortedListDemo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+
+namespace LearnCollections
+{
+    class SortedListDemo
+    {
+        static void Main()
+        {
+            SortedList sl = new SortedList();
+            sl.Add("building","living quarters");
+            sl.Add("car","transport");
+            sl.Add("book","kit typed word");
+            sl.Add("Apple","fruit");
+            sl["tractor"] = "agriculture machine";
+            ICollection c = sl.Keys;
+            foreach (string str in c)
+                Console.WriteLine(str + ": " + sl[str]);
+            Console.WriteLine();
+            Console.WriteLine("Content list by index");
+            for (int i = 0; i < sl.Count; i++)
+                Console.WriteLine(sl.GetByIndex(i));
+            Console.WriteLine();
+            Console.WriteLine("Integer index elements list");
+            foreach (string str in c)
+                Console.WriteLine(str + ": " + sl.IndexOfKey(str));
+            {
+                
+            }
+        }
+    }
+}
