901550fdcaca8c706d8dca7926af7a682bb0eb17
Kivi27 <kivi27072002@gmail.com>
2021-08-17 10:05:58 +0300

    Example of use sorted dictionary

---
+++ Other/Collections/Generic/SortedDictionaryDemo.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace LearnCollections
+{
+    class SortedDictionaryDemo
+    {
+        static void Main()
+        {
+            SortedDictionary<string, double> dict = new SortedDictionary<string, double>();
+            dict.Add("Batler, John", 73000);
+            dict.Add("Shwarz, Sara", 59000);
+            dict.Add("Paik, Tomas", 45000);
+            dict.Add("Frank, Ed", 99000);
+            ICollection<string> c = dict.Keys;
+            foreach (var str in c)
+                Console.WriteLine("{0}, salary: {1:C}",str,dict[str]);
+        }
+    }
+}
